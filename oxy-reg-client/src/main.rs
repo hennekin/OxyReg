79add23ecdc10d89d6aaa41d1feafa42b3b1000d
henne <hennekin@proton.me>
2026-07-24 20:16:24 +0200

    ✨ Add tcp stream that can send example message to server

--- oxy-reg-client/src/main.rs
+++ oxy-reg-client/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world from client!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()>{
+    let mut stream = TcpStream::connect("127.0.0.1:7000")?;
+
+    let msg_bytes = "Moin mein grosser".as_bytes();
+    for mb in msg_bytes {
+        stream.write(&[*mb])?;
+    }
+    // Alternative: stream.write_all(b"Moin mein grosser");
+
+    Ok(())
 }
