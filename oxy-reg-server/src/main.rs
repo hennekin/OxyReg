88bad704239f5377a8f27acf3156ae985f4dbd6a
Jakob Korries <jakob.korries@haw-hamburg.de>
2026-07-24 17:20:54 +0200

    ✨ add first tcp listening code

--- oxy-reg-server/src/main.rs
+++ oxy-reg-server/src/main.rs
@@ -1,3 +1,30 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_client(_stream: TcpStream) {
+    // hier müsste Zeileweise gelesen werden
+}
+
 fn main() {
-    println!("Hello, world from server!");
+    let tcp_listener;
+    let listener = TcpListener::bind("0.0.0.0:7000");
+    match listener {
+        Ok(l) => tcp_listener = l,
+        Err(e) => {
+            eprintln!("{}", e);
+            return;
+        }
+    }
+
+    // accept connections and process them serially
+    for stream in tcp_listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(_e) => {
+                println!("Something went wrong")
+            }
+        }
+    }
 }
